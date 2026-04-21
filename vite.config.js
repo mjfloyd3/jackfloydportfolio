217c39e8597cf65e3922e0d2311ee5fee20fb74e
Jack Floyd <mjfloyd3@gmail.com>
2026-04-21 19:57:43 -0400

    update independent projects

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
                 project1: 'project-1.html',
                 project2: 'project-2.html',
                 project3: 'project-3.html',
+                carlycaptions: 'carly-captions.html',
                 bmworkflowdiagram: 'bm-workflow-diagram.html',
             }
         }
